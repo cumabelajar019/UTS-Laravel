da746d4b43eb6dd8155620460cca8e566699e846
Ari <ariesyahputra9999@gmail.com>
2025-11-04 16:26:34 +0700

    Jawaban no 4

--- resources/views/tambah-produk.blade.php
+++ resources/views/tambah-produk.blade.php
@@ -1,26 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Form Tambah Produk</h1>
+<div class="container mt-4">
+    <h1 class="mb-4">Form Tambah Produk</h1>
 
-    <form action="#" method="POST" class="mt-4">
-        @csrf
-        <div class="mb-3">
-            <label for="kode" class="form-label">Kode Produk</label>
-            <input type="text" id="kode" name="kode" class="form-control" placeholder="Masukkan kode produk">
+    <form>
+        <!-- Baris Pertama -->
+        <div class="row g-3">
+            <div class="col-md-4">
+                <label for="kode" class="form-label">Kode Produk</label>
+                <input type="text" id="kode" class="form-control" placeholder="Input Kode Produk">
+            </div>
+            <div class="col-md-4">
+                <label for="nama" class="form-label">Nama Produk</label>
+                <input type="text" id="nama" class="form-control" placeholder="Input Nama Produk">
+            </div>
+            <div class="col-md-4">
+                <label for="jenis" class="form-label">Jenis Produk</label>
+                <select id="jenis" class="form-control">
+                    <option selected>Pilih Produk</option>
+                    <option>Alat Tulis</option>
+                    <option>Elektronik</option>
+                    <option>Lainnya</option>
+                </select>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="nama" class="form-label">Nama Produk</label>
-            <input type="text" id="nama" name="nama" class="form-control" placeholder="Masukkan nama produk">
+
+        <!-- Baris Kedua -->
+        <div class="row g-3 mt-3">
+            <div class="col-md-4">
+                <label for="harga" class="form-label">Harga</label>
+                <input type="number" id="harga" class="form-control" placeholder="Input Harga">
+            </div>
+            <div class="col-md-4 d-flex align-items-end">
+                <button type="submit" class="btn btn-success w-100">Simpan</button>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="jenis" class="form-label">Jenis Produk</label>
-            <input type="text" id="jenis" name="jenis" class="form-control" placeholder="Masukkan jenis produk">
-        </div>
-        <div class="mb-3">
-            <label for="harga" class="form-label">Harga</label>
-            <input type="number" id="harga" name="harga" class="form-control" placeholder="Masukkan harga produk">
-        </div>
-        <button type="submit" class="btn btn-success">Simpan</button>
     </form>
+</div>
 @endsection
